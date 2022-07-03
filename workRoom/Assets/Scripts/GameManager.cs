34ebd3db2945b58c5551f765346af39ed4fb84dc
Yunjae Jang <jjw1270@naver.com>
2022-07-03 22:45:15 +0900

    Create MainScene + GameMgr
    Base Scene

---
+++ workRoom/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public string nowSkillName = "";
+    public bool haveSkill() => nowSkillName == "" ? false : true;  // 현재 스킬을 보유하고있는지
+
+    public List<string> SkillNameSet = new List<string> { };
+    public GameObject[] SkillResource;
+    public int nowProgressLevel;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameData gameData;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+                Destroy(this.gameObject);
+        }
+        gameData = new GameData { nowSkillName = "", SkillNameSet = { "Fire", "Barrier", "Water" }, nowProgressLevel = 0 };
+        gameData.SkillResource = new GameObject[gameData.SkillNameSet.Count];
+        ResourceLoad();
+    }
+     public void ResourceLoad()
+    {
+        for (int i = 0; i < gameData.SkillNameSet.Count; i++)
+        {
+            gameData.SkillResource[i] = Resources.Load<GameObject>(string.Format("SkillEffect/{0}", gameData.SkillNameSet[i]));
+        }
+    }
+}
